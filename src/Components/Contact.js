63b77dc120b7dc38650059f6100aa2a08cc72bab
faris1208 <farislawal3@gmail.com>
2025-03-25 13:00:46 +0100

    links

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,18 +5,21 @@ function Contact() {
     <div className='Portfolio-Container'>
          <div className='Contacts' id='Contact'>
             <div className='Faris'>
-                <a href='https://myaccount.google.com/email'>farislawal3@gmail.com</a>
+                <a href='mailto:https://myaccount.google.com/email'>farislawal3@gmail.com</a>
             </div>
             <div className='Linkedin-head'>
                 {/* <p>Ogun State, Ng</p> */}
                 <div className='Linkedin'>
-                    <a href='https://mobile.twitter.com/@Olabodepharris1'>
+                    <a href='https://mobile.twitter.com/@Olabodepharris1' target="_blank"
+              rel="noopener noreferrer">
                         <button className='Twitter'>TWITTER</button>
                     </a>
-                    <a href='https//linkedin.com/in/faris-lawal-11b4972a6'>
+                    <a href='https//linkedin.com/in/faris-lawal-11b4972a6' target="_blank"
+              rel="noopener noreferrer">
                         <button className='Twitter'>LINKEDIN</button>
                     </a>
-                    <a href='https://github.com/faris1208'>
+                    <a href='https://github.com/faris1208' target="_blank"
+              rel="noopener noreferrer">
                         <button className='Twitter'>GITHUB</button>
                     </a>
                 </div>
@@ -25,13 +28,16 @@ function Contact() {
             <div className='Linkedin-head2'>
                 <p>Ogun State, Ng</p>
                 <div className='Linkedin'>
-                    <a href='https://mobile.twitter.com/@Olabodepharris1'>
+                    <a href='https://mobile.twitter.com/@Olabodepharris1' target="_blank"
+              rel="noopener noreferrer">
                         <button className='Twitter'>TWITTER</button>
                     </a>
-                    <a href='https://www.linkedin.com/in/faris-lawal-11b4972a6?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BMOaDj32rRuS0gBPO%2FALV3g%3D%3D'>
+                    <a href='https://www.linkedin.com/in/faris-lawal-11b4972a6?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BMOaDj32rRuS0gBPO%2FALV3g%3D%3D' target="_blank"
+              rel="noopener noreferrer">
                         <button className='Twitter'>LINKEDIN</button>
                     </a>
-                    <a href='https://github.com/faris1208'>
+                    <a href='https://github.com/faris1208' target="_blank"
+              rel="noopener noreferrer">
                         <button className='Twitter'>GITHUB</button>
                     </a>
                 </div>
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from "react";
 // import phone2 from "../assets/Img/pharris 001.jpeg";
-import { HashLink as Link } from 'react-router-hash-link';
+import { HashLink as Link } from "react-router-hash-link";
 import { FaReact } from "react-icons/fa";
 import { TbBrandNextjs } from "react-icons/tb";
 import { IoLogoJavascript } from "react-icons/io5";
@@ -10,210 +10,265 @@ import { SiSass } from "react-icons/si";
 import { SiTailwindcss } from "react-icons/si";
 import { SiStyledcomponents } from "react-icons/si";
 import { IoLogoHtml5 } from "react-icons/io5";
-import resume from "../assets/Img/Faris Lawal Resume.pdf"
-
+import resume from "../assets/Img/Faris Lawal Resume.pdf";
 
 function Portfolio() {
   return (
-    <div className='Portfolio-Container'>
-        
-        <nav className='Lawal-nav'>
-            <div className='Lawal'>
-                <p>LAWAL FARIS</p>
+    <div className="Portfolio-Container">
+      <nav className="Lawal-nav">
+        <div className="Lawal">
+          <p>LAWAL FARIS</p>
+        </div>
+        <div className="Resume">
+          <a href={resume} download="resume">
+            <button className="Resume-btn">Resume</button>
+          </a>
+          <Link to={"#Contact"} style={{ textDecoration: "none" }} smooth>
+            <a href="#Contacts">
+              <button className="Contact-btn">Contact</button>
+            </a>
+          </Link>
+        </div>
+      </nav>
+      <div className="Front-End">
+        <h1>FRONT-END</h1>
+        <h2>ENGINEER</h2>
+      </div>
+      <div className="About">
+        <p>
+          {" "}
+          <span className="bout">ABOUT</span> Hi, I'm Lawal Faris, a front-end
+          engineer experienced in writing clean and reusable code with the
+          latest tools.
+        </p>
+        <p>
+          My career goal is to develop processes and software tools that can
+          improve service delivery and product quality.
+        </p>
+      </div>
+      <Link to={"#My-Tools"} style={{ textDecoration: "none" }} smooth>
+        <div className="Scroll">
+          <p>Scroll down</p>
+        </div>
+      </Link>
+
+      <div className="Tools-container" id="My-Tools">
+        <div className="ools">
+          <p>Tools :</p>
+        </div>
+        <div className="Tools-wrapper">
+          <div className="Tools-header">
+            <div className="Tools-img">
+              <FaReact className="react-icon" />
+              <p>React.js</p>
             </div>
-            <div className='Resume'>
-                <a href={resume} download="resume">
-                    <button className='Resume-btn'>Resume</button>
+            <div className="Tools-img">
+              <TbBrandNextjs className="react-icon" />
+              <p>Next.js</p>
+            </div>
+            <div className="Tools-img">
+              <IoLogoJavascript className="react-icon" />
+              <p>Javascript</p>
+            </div>
+            <div className="Tools-img">
+              <SiTypescript className="react-icon" />
+              <p>Typescript</p>
+            </div>
+            <div className="Tools-img">
+              <SiCss3 className="react-icon" />
+              <p>Css3</p>
+            </div>
+            <div className="Tools-img">
+              <SiSass className="react-icon" />
+              <p>Sass</p>
+            </div>
+            <div className="Tools-img">
+              <SiTailwindcss className="react-icon" />
+              <p>Tailwindcss</p>
+            </div>
+            <div className="Tools-img-two">
+              <SiStyledcomponents className="react-icon" />
+              <p>Styled components</p>
+            </div>
+            <div className="Tools-img">
+              <IoLogoHtml5 className="react-icon" />
+              <p>Html5</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="Projects-container">
+        <div className="Projects-wrapper">
+          <div className="Projects-texts">
+            <div className="Featured">
+              <p>Featured</p>
+              <p>Project(5)</p>
+            </div>
+            <p>Nextjs,Typescript,Module Scss</p>
+          </div>
+          <div className="Projects-Sites">
+            <a
+              href="https://www.clubarant.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="Movies">
+                <h2>Clubarant</h2>
+              </div>
+            </a>
+            <div className="Visit">
+              <button className="visit-btn">
+                <a
+                  href="https://www.clubarant.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  VISIT SITE
                 </a>
-                <Link to={'#Contact'} style={{textDecoration: 'none'}} smooth>
-                    <a href='#Contacts'>
-                        <button className='Contact-btn'>Contact</button>
-                    </a>
-                </Link>
-                
+              </button>
             </div>
-        </nav>
-        <div className='Front-End'>
-            <h1>FRONT-END</h1>
-            <h2>ENGINEER</h2>
+          </div>
         </div>
-        <div className='About'>
-            <p> <span className='bout'>ABOUT</span>  Hi, I'm Lawal Faris, a front-end engineer experienced in writing clean and reusable code with the latest tools.</p>
-            <p>My career goal is to develop processes and software tools that can improve service delivery and product quality.</p>
-        </div>
-        <Link to={'#My-Tools'} style={{textDecoration: 'none'}} smooth>
-            <div className='Scroll'>
-                <p>Scroll down</p>
-            </div>
-        </Link>
+      </div>
 
-        <div className='Tools-container' id='My-Tools'>
-            <div className='ools'>
-                <p>Tools :</p>
-            </div>
-            <div className='Tools-wrapper'>
-                <div className='Tools-header'>
-                    <div className='Tools-img'>
-                        <FaReact className='react-icon' />
-                        <p>React.js</p>
-                    </div>
-                    <div className='Tools-img'>
-                        <TbBrandNextjs className='react-icon' />
-                        <p>Next.js</p>
-                    </div>
-                    <div className='Tools-img'>
-                        <IoLogoJavascript  className='react-icon' />
-                        <p>Javascript</p>
-                    </div>
-                    <div className='Tools-img'>
-                        <SiTypescript className='react-icon' />
-                        <p>Typescript</p>
-                    </div>
-                    <div className='Tools-img'>
-                        <SiCss3 className='react-icon' />
-                        <p>Css3</p>
-                    </div>
-                    <div className='Tools-img'>
-                        <SiSass className='react-icon' />
-                        <p>Sass</p>
-                    </div>
-                    <div className='Tools-img'>
-                        <SiTailwindcss className='react-icon' />
-                        <p>Tailwindcss</p>
-                    </div>
-                    <div className='Tools-img-two'>
-                        <SiStyledcomponents className='react-icon' />
-                        <p>Styled components</p>
-                    </div>
-                    <div className='Tools-img'>
-                        <IoLogoHtml5 className='react-icon' />
-                        <p>Html5</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div className='Projects-container'>
-            <div className='Projects-wrapper'>
-                <div className='Projects-texts'>
-                    <div className='Featured'>
-                        <p>Featured</p>
-                        <p>Project(5)</p>
-                    </div>
-                    <p>Nextjs,Typescript,Module Scss</p>
-                </div>
-                <div className='Projects-Sites'>
-                    <a href='https://www.clubarant.com/'>
-                        <div className='Movies'>
-                            <h2>Clubarant</h2>
-                        </div>
-                    </a>
-                    <div className='Visit'>
-                        <button className='visit-btn'>
-                            <a href='https://www.clubarant.com/'>VISIT SITE</a>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div className='Projects-container'>
-            <div className='Projects-wrapper'>
-                <div className='Projects-texts-two'>
-                    {/* <div className='Featured'>
+      <div className="Projects-container">
+        <div className="Projects-wrapper">
+          <div className="Projects-texts-two">
+            {/* <div className='Featured'>
                         <p>Featured</p>
                         <p>Project(5)</p>
                     </div> */}
-                    <p>Next.Js,Scss</p>
-                </div>
-                <div className='Projects-Sites'>
-                    <a href='https://bricklage.vercel.app/'>
-                        <div className='Movies'>
-                            <h2>Bricklage - Project</h2>
-                        </div>
-                    </a>
-                    <div className='Visit'>
-                        <button className='visit-btn'>
-                            <a href='https://bricklage.vercel.app/'>VISIT SITE</a>
-                        </button>
-                    </div>
-                </div>
+            <p>Next.Js,Scss</p>
+          </div>
+          <div className="Projects-Sites">
+            <a
+              href="https://bricklage.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="Movies">
+                <h2>Bricklage - Project</h2>
+              </div>
+            </a>
+            <div className="Visit">
+              <button className="visit-btn">
+                <a
+                  href="https://bricklage.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  VISIT SITE
+                </a>
+              </button>
             </div>
+          </div>
         </div>
+      </div>
 
-        <div className='Projects-container'>
-            <div className='Projects-wrapper'>
-                <div className='Projects-texts-two'>
-                    {/* <div className='Featured'>
+      <div className="Projects-container">
+        <div className="Projects-wrapper">
+          <div className="Projects-texts-two">
+            {/* <div className='Featured'>
                         <p>Featured</p>
                         <p>Project(5)</p>
                     </div> */}
-                    <p>Nextjs,Module Scss</p>
-                </div>
-                <div className='Projects-Sites'>
-                    <a href='https://www.maritestafrica.com/'>
-                        <div className='Movies'>
-                            <h2>Maritest - Project</h2>
-                        </div>
-                    </a>
-                    <div className='Visit'>
-                        <button className='visit-btn'>
-                            <a href='https://www.maritestafrica.com/'>VISIT SITE</a>
-                        </button>
-                    </div>
-                </div>
+            <p>Nextjs,Module Scss</p>
+          </div>
+          <div className="Projects-Sites">
+            <a
+              href="https://www.maritestafrica.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="Movies">
+                <h2>Maritest - Project</h2>
+              </div>
+            </a>
+            <div className="Visit">
+              <button className="visit-btn">
+                <a
+                  href="https://www.maritestafrica.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  VISIT SITE
+                </a>
+              </button>
             </div>
+          </div>
         </div>
+      </div>
 
-        <div className='Projects-container'>
-            <div className='Projects-wrapper'>
-                <div className='Projects-texts-two'>
-                    {/* <div className='Featured'>
+      <div className="Projects-container">
+        <div className="Projects-wrapper">
+          <div className="Projects-texts-two">
+            {/* <div className='Featured'>
                         <p>Featured</p>
                         <p>Project(5)</p>
                     </div> */}
-                    <p>React,Scss,Html</p>
-                </div>
-                <div className='Projects-Sites'>
-                    <a href='https://rock-bottom.vercel.app/'>
-                        <div className='Movies'>
-                            <h2>Rock Bottom Initiative</h2>
-                        </div>
-                    </a>
-                    <div className='Visit'>
-                        <button className='visit-btn'>
-                            <a href='https://rock-bottom.vercel.app/'>VISIT SITE</a>
-                        </button>
-                    </div>
-                </div>
+            <p>React,Scss,Html</p>
+          </div>
+          <div className="Projects-Sites">
+            <a
+              href="https://rock-bottom.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="Movies">
+                <h2>Rock Bottom Initiative</h2>
+              </div>
+            </a>
+            <div className="Visit">
+              <button className="visit-btn">
+                <a
+                  href="https://rock-bottom.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  VISIT SITE
+                </a>
+              </button>
             </div>
+          </div>
         </div>
+      </div>
 
-        <div className='Projects-container-two'>
-            <div className='Projects-wrapper'>
-                <div className='Projects-texts-two'>
-                    {/* <div className='Featured'>
+      <div className="Projects-container-two">
+        <div className="Projects-wrapper">
+          <div className="Projects-texts-two">
+            {/* <div className='Featured'>
                         <p>Featured</p>
                         <p>Project(5)</p>
                     </div> */}
-                    <p>Javascript,Css,Html</p>
-                </div>
-                <div className='Projects-Sites'>
-                    <a href='https://faris1208.github.io/Search-Engine/'>
-                        <div className='Movies'>
-                            <h2>Search - Engine</h2>
-                        </div>
-                    </a>
-                    <div className='Visit'>
-                        <button className='visit-btn'> 
-                            <a href='https://faris1208.github.io/Search-Engine/'>VISIT SITE</a>
-                        </button>
-                    </div>
-                </div>
+            <p>Javascript,Css,Html</p>
+          </div>
+          <div className="Projects-Sites">
+            <a
+              href="https://faris1208.github.io/Search-Engine/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="Movies">
+                <h2>Search - Engine</h2>
+              </div>
+            </a>
+            <div className="Visit">
+              <button className="visit-btn">
+                <a
+                  href="https://faris1208.github.io/Search-Engine/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  VISIT SITE
+                </a>
+              </button>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Portfolio
+export default Portfolio;
